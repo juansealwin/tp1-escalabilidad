514827ac5f02fbe4f8f37015fe0cf82f51d617cb
Juan Alvarez Windey <juan.alvarezwindey@gmail.com>
2024-04-23 20:04:07 -0300

    Setup first queue

--- client/main.py
+++ client/main.py
@@ -1,10 +1,37 @@
 import pika
+import time
+import os
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+filename = 'data/books_data.csv'
+
+def send_message(channel, message):
+    channel.basic_publish(exchange='', routing_key='books_analizer', body=message)
+    print(f" [x] Sent '{message}'")
+
+print(' [*] Waiting for RabbitMQ to start...')
+while True:
+    try:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+        break
+    
+    except pika.exceptions.AMQPConnectionError:
+        print(' [!] RabbitMQ not available yet, waiting...')
+        time.sleep(2)
+
+print(' [*] Connected to RabbitMQ')
 channel = connection.channel()
+channel.queue_declare(queue='books_analizer', durable=True)
 
-channel.queue_declare(queue='hello')
+if os.path.isfile(filename):
+    with open(filename, 'r') as file:
+        for line in file:
+            msg = line.strip() 
+            send_message(channel, msg)
+            time.sleep(1)  
 
-channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
-print(" [x] Sent 'Hello World!'")
-connection.close()
+else:
+    print(' [!] File not found: data/books_data.csv')
+
+
+if connection.is_open:
+    connection.close()
--- server/main.py
+++ server/main.py
@@ -1,25 +1,26 @@
-import pika, sys, os
+import pika, time
 
 def callback(ch, method, properties, body):
     print(f" [x] Received {body}")
 
-def main():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
-    channel = connection.channel()
-
-    channel.queue_declare(queue='hello')
-
-    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
-
-    print(' [*] Waiting for messages. To exit press CTRL+C')
-    channel.start_consuming()
-
-if __name__ == '__main__':
+print(' [*] Waiting for RabbitMQ to start...')
+while True:
     try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
+        break
+    except pika.exceptions.AMQPConnectionError:
+        print(' [!] RabbitMQ not available yet, waiting...')
+        time.sleep(1)
+
+print(' [*] Connected to RabbitMQ')
+
+channel = connection.channel()
+channel.queue_declare(queue='books_analizer', durable=True)
+#channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='books_analizer', on_message_callback=callback, auto_ack=True)
+
+
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+channel.start_consuming()
+
