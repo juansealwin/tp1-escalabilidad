6e2e354f131ce32e34987a5d45f4556123f3f206
LucasMontenegro1 <lu.montenegro98@gmail.com>
2024-05-11 15:07:17 -0300

    refactor review couter

--- review_counter/review_counter.py
+++ review_counter/review_counter.py
@@ -5,7 +5,7 @@ import pika
 import logging
 import signal
 from common.log import init_log
-
+from rabbitmq.queue_manager import *
 
 class ReviewCounter():
     TITLE_POS = 0
@@ -14,63 +14,41 @@ class ReviewCounter():
         self.counter = {}
         self.__init_config()
         time.sleep(10)
-        self.__connect_to_rabbitmq()
+        self.queue_manager = QueueManager(single_channel=True)
         self.__setup_queues()
         signal.signal(signal.SIGTERM, self.handle_sigterm)
         self.shutdown_requested = False
 
     def __init_config(self):
         log_level = os.getenv("LOG_LEVEL", "INFO")
-        init_log(log_level)
         self.pair_count = int(os.getenv("PAIR_COUNT", '0'))
         self.leader = int(os.getenv("LEADER",'0'))
-
-        logging.info(self.leader)
         self.id = int(os.getenv("ID",'0'))
-        logging.info(self.id)
-
-
-    def __connect_to_rabbitmq(self):
-        logging.info(' [*] Waiting for RabbitMQ to start...')
-        while True:
-            try:
-                self.__connection = pika.BlockingConnection(
-                    pika.ConnectionParameters(host='rabbitmq'))
-                break
-            except pika.exceptions.AMQPConnectionError:
-                logging.info(' [!] RabbitMQ not available yet, waiting...')
-                time.sleep(1)
-
-        logging.info(' [*] Connected to RabbitMQ')
+        init_log(log_level)
 
     def __setup_queues(self):
-        self.__channel = self.__connection.channel()
-        self.__channel.queue_declare(queue='rating_data', durable=True)
-        self.__channel.basic_consume(queue='rating_data', on_message_callback=self.__process_message, auto_ack=True)
-        self.__channel.queue_declare(queue='review_counter', durable=True)
+        self.queue_manager.setup_receive_queue('rating_data',callback=self.__process_message, auto_ack=True, durable=True)
+        self.queue_manager.setup_send_queue('review_counter',durable=True)
         self.__setup_leader_queues()
     
     def __setup_leader_queues(self):
         if self.id == self.leader:
-            logging.info("Leader setting up channels")
-            self.__channel.queue_declare(queue='leader_finish')
-            self.__channel.basic_consume(queue='leader_finish' , on_message_callback = self.__process_finish_message, auto_ack = True)
+            self.queue_manager.setup_receive_queue('leader_finish', callback= self.__process_finish_message, auto_ack=True, durable=True)
         else:
-            logging.info("Setting up finish channel")
-            self.__channel.queue_declare(queue='leader_finish')
-            self.__channel = self.__connection.channel()
-            self.__channel.queue_declare(queue=f'leader_finish_{self.id}', durable=True)
-            self.__channel.basic_consume(queue=f'leader_finish_{self.id}',on_message_callback = self.__process_finish_message, auto_ack = True)
-            
+            self.queue_manager.setup_send_queue('leader_finish',durable=True)
+            self.queue_manager.setup_receive_queue(f'leader_finish_{self.id}',callback=self.__process_finish_message,auto_ack=True, durable=True)            
+
 
     def __process_message(self, ch, method, properties, body):
         if self.shutdown_requested:
             return
+        
         line = body.decode('utf-8')
+
         if line == "END":
             logging.info(f"sending ending signal to all {self.pair_count} pairs")
             if self.id != self.leader:
-                self.__send_message(self.__channel, f"END,{self.id}", 'leader_finish')
+                self.queue_manager.send_message('leader_finish' ,f"END,{self.id}")
                 self.__forward_message()
                 return
             else:
@@ -78,10 +56,10 @@ class ReviewCounter():
                 self.__forward_message()
                 return
 
-        fields = line.split(',')
+        fields = line.split('|')
         self.counter.setdefault(fields[self.TITLE_POS], 0)
         self.counter[fields[self.TITLE_POS]] += 1
-        logging.debug(f"processed {body}")
+        logging.debug(f"processed {fields[self.TITLE_POS]}")
 
     def __process_finish_message(self, ch, method, properties, body):
         line = body.decode('utf-8')
@@ -99,20 +77,15 @@ class ReviewCounter():
         logging.info("Letting everyone know")
         for i in range(0,self.pair_count):
             if i != self.id and i!= sender:
-               actual_connection = self.__connection.channel()
-               actual_channel = actual_connection.queue_declare(queue=f'leader_finish_{i}',durable=True)
-               self.__send_message(actual_connection, f"END", f'leader_finish_{i}')
+                self.queue_manager.setup_send_queue(f'leader_finish_{i}',durable=True)
+                self.queue_manager.send_message(f'leader_finish_{i}',f"END")
     
     def __forward_message(self):
+        logging.info(f"{self.counter}")
         for key in self.counter.keys():
-            logging.debug(f'key')
-            self.__send_message(self.__channel, f"{key},{self.counter[key]}", 'review_counter')
+            self.queue_manager.send_message('review_counter',f"{key},{self.counter[key]}")
         
-        self.__send_message(self.__channel, f"END", 'review_counter')
-
-    def __send_message(self, channel, message, routing_key):
-        channel.basic_publish(exchange='', routing_key=routing_key, body=message)
-        logging.info(f" [x] Sent '{message}'")
+        self.queue_manager.send_message('review_counter',f"END")
 
     def handle_sigterm(self, signum, frame):
         logging.debug('action: handle_sigterm | result: in_progress')
@@ -121,5 +94,5 @@ class ReviewCounter():
 
     def run(self):
         logging.info(' [*] Waiting for messages. To exit press CTRL+C')
-        self.__channel.start_consuming()
+        self.queue_manager.start_consuming_sq()
         
